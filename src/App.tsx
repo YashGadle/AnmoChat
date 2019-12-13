dd1cb73d43153bbb4e0c50c2f366f5135bf76444
yash <yashgadle@gmail.com>
2019-12-14 00:17:52 +0530

    Removed dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
 import React, { Fragment } from 'react';
 
-import {
-	SafeAreaView,
-	StyleSheet,
-	ScrollView,
-	View,
-	Text,
-	StatusBar
-} from 'react-native';
-
-import {
-	Header,
-	LearnMoreLinks,
-	Colors,
-	DebugInstructions,
-	ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, Text, StatusBar } from 'react-native';
 
 const App = () => {
 	return (
