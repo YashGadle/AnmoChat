e9529e038d8bb4eb6c39f47176c74dc095384fab
yash <yashgadle@gmail.com>
2019-12-16 12:18:04 +0530

    Basic authenticate flow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-import { SafeAreaView, Text, StatusBar,View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-import { NavigationNativeContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import {ApplicationProvider} from '@ui-kitten/components';
+import { ApplicationProvider } from '@ui-kitten/components';
 import { mapping, dark as darkTheme } from '@eva-design/eva';
 
-import Login from './screens/Login';
-
+import AuthNavigator from './navigators/Auth';
 import { ApolloAPI } from './components/Apollo';
-
-const Stack = createStackNavigator();
 
 const appTheme = require('./custom-theme.json');
 const theme = {...darkTheme, ...appTheme}
 
-const Ex = () => (
-	<View style={{flex:1,alignItems: 'center', justifyContent: 'center' }}>
-		<Text>Yash gadle</Text>
-	</View>
-)
-
 const App = () => {
 	return (
 		<ApplicationProvider mapping={mapping} theme={theme}>
-		<ApolloAPI token="">
-			<StatusBar barStyle="dark-content" />
-				<NavigationNativeContainer>
-					<Stack.Navigator initialRouteName='LoginScreen'>
-						<Stack.Screen name="LoginScreen" component={Login} />
-						<Stack.Screen name="Ex" component={Ex} />
-					</Stack.Navigator>
-				</NavigationNativeContainer>
-		</ApolloAPI>
+			<ApolloAPI token="">
+				<StatusBar barStyle="dark-content" />
+				<AuthNavigator />
+			</ApolloAPI>
 		</ApplicationProvider>
 	);
 };
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+
+import { SafeAreaView } from 'react-native';
+
+import { Text, Layout } from '@ui-kitten/components';
 
 interface Props {}
 
 export const Login: React.FC<Props> = props => {
 	const {} = props;
 
-	const { data, loading, error } = useQuery(gql`
-		query MyQuery {
-			user {
-				id
-			}
-		}
-	`);
-
-	if (loading) return <Text>Loading...</Text>;
-
-	if (error) return <Text>Error...{error.message}</Text>;
-
 	return (
-		<View>
-			<Text>{JSON.stringify(data)}</Text>
-		</View>
+		<Layout style={{ flex: 1 }}>
+			<SafeAreaView style={{ flex: 1 }}>
+				<Text>Hi This is Login!</Text>
+			</SafeAreaView>
+		</Layout>
 	);
 };
 
